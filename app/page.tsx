83e9e85e09b1b8cec3a1a38f21abfbdbb79aed75
Joaquin I. Ledesma Sedran <jledesma@creminox.com>
2026-04-10 13:00:30 -0300

    general

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import Image from "next/image";
-import General from "@/public/designs/General.png";
+import ARMADOR_D from "@/public/general/ARMADOR_D.png";
+import ARMADOR_L from "@/public/general/ARMADOR_L.png";
 import DatosGeneral from "@/components/general/sectorDatosGeneral";
 import { useTranslation } from "react-i18next";
+import { useThemeToggle } from "@/components/theme/themeToggleLogic";
 
 export default function Home() {
   const { t } = useTranslation();
+  const { theme } = useThemeToggle();
+
+  const imageSource = theme === "dark" ? ARMADOR_D : ARMADOR_L;
 
   return (
     <div className="flex flex-col gap-5 p-5 w-full">
@@ -13,7 +18,7 @@ export default function Home() {
         <div className="w-3/4 relative shrink-0">
           <Image
             alt={t("mayus.general")}
-            src={General}
+            src={imageSource}
             className="w-full h-auto object-contain"
             priority
           />
